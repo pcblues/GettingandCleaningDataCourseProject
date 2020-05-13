c54fc1bb3cfc100d9f3958407a6f2999012b2af9
Mark D Osborne <markosborne@pcblues.com>
2020-05-13 10:32:21 +1000

    Initial commit, barebones file reqs

---
+++ README.md
@@ -0,0 +1,7 @@
+Training and test data from the accelerometer and Samsung Galaxy 5 
+
+Attribution  
+
+Dataset used in this analyses from:
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using Smartphones. 21th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium 24-26 April 2013. 
---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+# Project
+# Author: Mark Osborne
+
+# install codebook package?
+# create github repository
+# include attribution in README.md
+
+
+# Merge training and test sets (y and X)
+## Load data sets from files (4)
+## Merge four sets into one
+
+
+
+# Take out mean and std for each measurement
+## What are the columns
+
+# Name activities in data set (y)
+## Use activities file 
+
+# label dataset variables with good names
+## use features file to identify columns
+
+# Create new tidy data set with average of each variable for each activity and each subject
+## use write.table(), row.name=FALSE
+
+# upload to assignment page
+
+
+
+
